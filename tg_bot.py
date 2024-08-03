48e708155c3ed3ebe035a26af4c03783616ffee7
psyfluid <psyfluid@gmail.com>
2024-08-04 02:06:52 +0300

    model experiments

--- tg_bot.py
+++ tg_bot.py
@@ -3,10 +3,27 @@ import re
 from pathlib import Path
 
 import telebot
-import pytesseract
 from PIL import Image
 
+import pytesseract
+
+# from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
+# from transformers import AutoProcessor, AutoModelForVision2Seq
+
+# import easyocr
+
 from config import TOKEN
+
+# reader = easyocr.Reader(['ru'])
+
+# processor = TrOCRProcessor.from_pretrained("raxtemur/trocr-base-ru")
+# model = VisionEncoderDecoderModel.from_pretrained("raxtemur/trocr-base-ru")
+
+# MODEL_NAME = "HuggingFaceM4/idefics2-8b"
+# processor = AutoProcessor.from_pretrained(MODEL_NAME)
+# model = AutoModelForVision2Seq.from_pretrained(MODEL_NAME).to("cpu")
+
 
 Path('photos').mkdir(exist_ok=True)
 
@@ -14,7 +31,9 @@ ARROW_SYMBOL = '\u2198\uFE0F'
 
 bot = telebot.TeleBot(TOKEN)
 
-telebot.logging.basicConfig(filename='log.csv', level=logging.INFO, encoding='utf-8',
+telebot.logging.basicConfig(filename='log.csv',
+                            level=logging.INFO,
+                            encoding='utf-8',
                             format=' %(asctime)s; %(levelname)s; %(message)s')
 
 logger = telebot.logger
@@ -26,6 +45,15 @@ bot.set_my_commands(
         telebot.types.BotCommand('photo', 'отправить фото состава')
     ],
 )
+
+
+# def ocr_image(image_path):
+#     image = Image.open(image_path).convert("RGB")
+#     pixel_values = processor(image, return_tensors="pt").pixel_values
+#     generated_ids = model.generate(pixel_values)
+#     generated_text = processor.batch_decode(
+#         generated_ids, skip_special_tokens=True)[0]
+#     return generated_text
 
 
 def log_message(msg, txt):
@@ -47,7 +75,9 @@ def start_message(message):
 def photo_message(message):
     log_message(message, message.text)
     bot.send_message(
-        message.chat.id, f'Пришлите фото для определения состава продуктов {ARROW_SYMBOL}')
+        message.chat.id,
+        f'Пришлите фото для определения состава продуктов {ARROW_SYMBOL}'
+    )
 
 
 @bot.message_handler(content_types=['photo'])
@@ -62,9 +92,31 @@ def photo_reply(message):
 
     text = pytesseract.image_to_string(Image.open(file_name), lang='rus')
 
-    cleaned_text = re.sub(r'[^\w\s]', '', text)
-    words = re.findall(r'\b\w+\b', cleaned_text)
-    bot.reply_to(message, '\n'.join(words))
+    # TrOCR:
+    # text = ocr_image(file_name)
+    # print(text)
+
+    # easyOCR:
+    # result = reader.readtext(file_name)
+    # text = "\n".join([item[1] for item in result])
+
+    # Idefics2
+    # image = Image.open(file_name)
+
+    # # Prepare inputs for the model
+    # inputs = processor(images=image, return_tensors="pt").to(model.device)
+    # # generated_ids = model.generate(inputs.input_ids, attention_mask=inputs.attention_mask)
+    # generated_ids = model.generate(**inputs)
+    # generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+    # text = generated_texts[0]
+
+    text = re.sub(r'-\n', '', text)
+    text = re.sub(r'\n', ' ', text)
+    text = re.sub(r'\s-\s', ':', text)
+    cleaned_text = re.sub(r'[^\w\s\-,\.;\:\(\)\[\]]', '', text)
+    ingredients = [item.strip().lower()
+                   for item in re.split(r'[,\.;\:\(\)\[\]]+', cleaned_text)]
+    bot.reply_to(message, '\n'.join(ingredients))
 
 
 @bot.message_handler(content_types=['text'])
